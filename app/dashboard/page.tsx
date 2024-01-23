88e5814595dad2f578925270696a24bed1a5e372
unknown <marcoscab.tkd@gmail.com>
2024-01-23 03:29:27 -0300

    agregado pepe

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,12 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+import TaskForm from '@/app/ui/tasks/task-form';
  
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
+  const pepe = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -25,6 +27,7 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {<RevenueChart revenue={revenue}  />}
         {<LatestInvoices latestInvoices={latestInvoices} />}
+        {<TaskForm pepe={latestInvoices} />}
       </div>
     </main>
   );
