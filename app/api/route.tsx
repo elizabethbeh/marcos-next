005bb93f86796b2287f987a94abdbe6496d40441
marcosmdc <marcoscab.tkd@gmail.com>
2024-01-29 16:35:58 -0300

    agregado noStore

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 //import { fetchRevenue } from "@/app/lib/data";
 import { fetchTurnos } from "@/app/lib/data";
-export const dynamic = 'force-dynamic'
-//import { unstable_noStore as noStore } from 'next/cache';
+//export const dynamic = 'force-dynamic'
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function GET(){
-    //noStore();
+    noStore();
     //const turnos = await fetchRevenue();
     const turnos = await fetchTurnos();
     //return NextResponse.json({ data: turnos }, { status: 201 });
