0afa19246d76b5f38d7280797cce23860690c97b
marcosmdc <marcoscab.tkd@gmail.com>
2024-01-29 16:16:51 -0300

    agregado no cache

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -5,5 +5,14 @@ import { fetchTurnos } from "@/app/lib/data";
 export async function GET(){
     //const turnos = await fetchRevenue();
     const turnos = await fetchTurnos();
-    return NextResponse.json({ data: turnos }, { status: 201 });
+    //return NextResponse.json({ data: turnos }, { status: 201 });
+
+    //const turnos = await fetchTurnos();
+
+    return NextResponse.json({ data: turnos }, { 
+        status: 201,
+        headers: {
+            'Cache-Control': 'no-store',
+        },
+    });
 }
